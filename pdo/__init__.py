5470017e7e67d5952b19a8e4e36b60939c3d6aea
LavaPower <lavapower84@gmail.com>
2020-09-24 01:01:56 +0200

    Update version

--- pdo/__init__.py
+++ pdo/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "1.0.0"
-__all__ = ["PDO", "PDOException", "PDOConnectionException"]
+__version__ = "1.1.0"
+__all__ = ["PDO", "PDOException", "PDOConnectionException", "PDOSQLException"]
 
 from pdo.pdo import PDO
-from pdo.exception import PDOException, PDOConnectionException
+from pdo.exception import PDOException, PDOConnectionException, PDOSQLException
